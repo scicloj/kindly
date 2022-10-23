d61e3fd80da7a777e18bde7e6076a27cfd8cfc1c
daslu <daniel.slutsky@gmail.com>
2022-10-23 17:55:11 +0300

    docs

--- notebooks/custom_default_advice.clj
+++ notebooks/custom_default_advice.clj
@@ -1,6 +1,7 @@
-(ns default-advice
+(ns custom-default-advice
   (:require [scicloj.kindly.v3.api :as kindly]
             [scicloj.kindly.v3.kind :as kind]
+            [scicloj.kindly.v3.kindness :as kindness]
             [scicloj.kindly.v3.defaults :as defaults]))
 
 ;; ## Extending the default advice with predicates
@@ -11,3 +12,16 @@
                       :kind/three]]}))
 
 (kindly/advice {:value 3})
+
+
+;; ## Extending the default advice with types
+
+(deftype MyType1 [])
+
+(extend-protocol kindness/Kindness
+  MyType1
+  (kind [this]
+    :kind/abcd))
+
+
+(kindly/advice {:value (MyType1.)})
